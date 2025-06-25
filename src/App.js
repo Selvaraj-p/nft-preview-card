0002db0253f6f8a8c48250b7aa089ab4ba8f7d87
P Selvaraj <selvaraj.p88@gmail.com>
2025-06-25 23:16:26 +0530

    NFT Preview Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import nftImage from './image-equilibrium.jpg';
+import creatorAvatar from './image-avatar.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+   return (
+    <div className="card">
+      <div className="card__images">
+        <img src={nftImage} alt="Equilibrium" />
+        <div className="card__images-layer">
+          <img src="./images/icon-view.svg" alt="View icon" />
+        </div>
+      </div>
+
+      <div className="card__title">
+        <h1>Equilibrium #3429</h1>
+        <p>Our Equilibrium collection promotes balance and calm.</p>
+      </div>
+
+      <div className="card__time">
+        <div className="card__time-left">
+          <img src="./images/icon-ethereum.svg" alt="Ethereum" />
+          <span>0.041 ETH</span>
+        </div>
+        <div className="card__time-right">
+          <img src="./images/icon-clock.svg" alt="Clock" />
+          <span>3 days left</span>
+        </div>
+      </div>
+
+      <div className="card__creator">
+        <img src={creatorAvatar} alt="Creator" />
+        <p><span>Creation of</span> Jules Wyvern</p>
+      </div>
+
+      <div className="attribution">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Challenge by <a href="https://www.crio.do" target="_blank" rel="noopener noreferrer">Crio</a>.
+          Coded by <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">You</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
