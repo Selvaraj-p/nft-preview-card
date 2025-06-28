05b816dc2087ab8d34a7ff8f3c9803b5d3970b64
P Selvaraj <selvaraj.p88@gmail.com>
2025-06-28 20:05:17 +0530

    design changed

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Frame from "./Frame";
+
+const App = () => {
+    return (
+        <div>
+            <Frame />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div class="attribution">
+        Challenge by <a href="https://www.crio.do/" target="_blank">Crio.do</a>. 
+        Coded by <a href="https://www.github.com/selvaraj-p">selvaraj-p</a>.
+      </div>
+    );
+}
+
+export default Footer;
