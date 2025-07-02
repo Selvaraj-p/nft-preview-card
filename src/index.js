d1018724c1a6bd2c35a1da9e1698f560e89eb9ca
P Selvaraj <selvaraj.p88@gmail.com>
2025-07-02 21:15:57 +0530

    changes added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import * as React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App"; // Root component that renders your Preview Card
-import "./index.css"; // Global styles including font and layout
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
