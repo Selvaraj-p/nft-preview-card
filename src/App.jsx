8665758d7cb7af54f6bee4d1808db72fbb4cf94d
P Selvaraj <selvaraj.p88@gmail.com>
2025-07-02 21:48:12 +0530

    changes added

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@ function App() {
   return (
     <div className="app">
       <NFTCard />
-      <footer className="attribution">
-        Challenge by <a href="https://www.crio.do" target="_blank" rel="noopener noreferrer">Crio</a>.
-        Coded by <a href="https://www.github.com/selvaraj-p" target="_blank" rel="noopener noreferrer">selvaraj-p</a>.
-      </footer>
+
     </div>
   );
 }
--- src/NFTCard.jsx
+++ src/NFTCard.jsx
@@ -18,13 +18,13 @@ const NFTCard = () => {
           <img src={EquipIco} alt="View icon" />
         </div>
       </div>
-
-      <h1 className="card__title">Equilibrium #3429</h1>
-      <p className="card__description">
-        Our Equilibrium collection promotes balance and calm.
-      </p>
-
-      <div className="card__time">
+    <div className="card__title">
+          <h1>Equilibrium #3429</h1>
+          <p className="card__description">
+            Our Equilibrium collection promotes balance and calm.
+          </p>
+    </div>  
+    <div className="card__time">
         <div className="card__time-left">
           <img src={Ether} alt="Ethereum" />
           <span>0.041 ETH</span>
@@ -36,12 +36,15 @@ const NFTCard = () => {
       </div>
 
       <div className="card__creator">
-        <img src={AVR} alt="Creator" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREURSKMljVXEm57DuInU5lTToyHcaiYmWnmA&s" alt="Creator" />
         <span>
           Creation of <strong>selvaraj-p</strong>
         </span>
       </div>
-
+      <footer className="attribution">
+        Challenge by <a href="https://www.crio.do" target="_blank" rel="noopener noreferrer">Crio</a>.
+        Coded by <a href="https://github.com/selvaraj-p" target="_blank" rel="noopener noreferrer">selvaraj-p</a>.
+      </footer>
 
     </main>
   );
